21666325c02deb179e962d057a2f9c05999f0a0b
gladys kosam <ggladyskosam@gmail.com>
2023-09-11 08:06:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main()
+
+/**
+ *main -  This function is used to print an output
+ *
+ * Return : 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
